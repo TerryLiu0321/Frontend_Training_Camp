730d083e0629c8551bcb89e6b47b20c94ebc4008
taiyang.liu <taiyang.liu@thoughtworks.com>
2021-09-07 17:22:01 +0800

    [feat]: make up client.js

--- browser_operating_priciple/client.js
+++ browser_operating_priciple/client.js
@@ -104,8 +104,7 @@ class ResponseParser {
                 }
                 this.current = this.WAITING_STATUS_LINE_END;
             } else {
-                this.length *= 16;
-                this.statusLine += parseInt(char, 16);
+                this.statusLine += char;
             } 
         } else if(this.current === this.WAITING_STATUS_LINE_END) {
             if(char === '\n') {
@@ -122,16 +121,76 @@ class ResponseParser {
             } else {
                 this.headerName += char;
             }
-        } else if(this.current === this.WAITING_HEADER_LINE_END) {
+        } else if(this.current === this.WAITING_HEADER_SPACE) {
+            if(char === ' ') {
+                this.current = this.WAITING_HEADER_VALUE;
+            }
+        } else if(this.current === this.WAITING_HEADER_VALUE) {
+            if(char === '\r') {
+                this.current = this.WAITING_HEADER_LINE_END;
+                this.headers[this.headerName] = this.headerValue;
+                this.headerName = '';
+                this.headerValue = '';
+            } else {
+                this.headerValue += char;
+            }
+        } else if(this.current = this.WAITING_HEADER_LINE_END) {
             if(char === '\n') {
                 this.current = this.WAITING_HEADER_NAME;
             }
-        } else if(this.current === this.WAITING_HEADER_BLOCK_END) {
+        } else if(this.current = this.WAITING_HEADER_BLOCK_END) {
             if(char === '\n') {
                 this.current = this.WAITING_BODY;
             }
         } else if(this.current === this.WAITING_BODY) {
             this.bodyParser.receiveChar(char);
+        }
+    }
+}
+
+class TrunkedBodyParser {
+    constructor(){
+        this.WAITING_LENGTH = 0;
+        this.WAITING_LENGTH_LINE_END = 1;
+        this.READING_TRUNK = 2;
+        this.WAITING_NEW_LINE = 3;
+        this.WAITING_NEW_LINE_END = 4;
+
+        this.length = 0;
+        this.content = [];
+        this.isFinished = false;
+        this.current = this.WAITING_LENGTH;
+    }
+
+    receiveChar(char) {
+        if(this.current === this.WAITING_LENGTH) {
+            if(char === '\r') {
+                if(this.length === 0) {
+                    this.isFinished = true;
+                }
+                this.current = this.WAITING_LENGTH_LINE_END;
+            } else {
+                this.length *= 16;
+                this.length += parseInt(char, 16);
+            } 
+        } else if(this.current === this.WAITING_LENGTH_LINE_END) {
+            if(char === '\n') {
+                this.current = this.READING_TRUNK;
+            }
+        } else if(this.current === this.READING_TRUNK) {
+            this.content.push(char);
+            this.length--;
+            if(this.length === 0) {
+                this.current = this.WAITING_NEW_LINE;
+            }
+        } else if(this.current === this.WAITING_NEW_LINE) {
+            if(char === '\r') {
+                this.current = this.WAITING_NEW_LINE_END;
+            }
+        } else if(this.current === this.WAITING_NEW_LINE_END) {
+            if(char === '\n') {
+                this.current === this.WAITING_LENGTH;
+            }
         }
     }
 }
