1630fa2e7c697665a3ce12e8d8e4722085b8c566
taiyang.liu <taiyang.liu@thoughtworks.com>
2021-09-10 11:47:11 +0800

    [feat] modify code to make it run

--- browser_operating_priciple/server.js
+++ browser_operating_priciple/server.js
@@ -1,17 +1,21 @@
-const { on } = require('events');
-const http = require('http');
+const http = require("http");
 
-http.createServer((request, response) => {
+http
+  .createServer((request, response) => {
     let body = [];
-    http.request.on('error', err => {
+    request
+      .on("error", (err) => {
         console.error(err);
-    }).on('data', chunk => {
+      })
+      .on("data", (chunk) => {
         body.push(chunk);
-    }).on('end', () => {
+      })
+      .on("end", () => {
         body = Buffer.concat(body).toString();
-        console.log('body:', body);
-        response.writeHead(200, {'Content-Type': 'text/html'});
-    });
-}).listen(8088);
+        console.log("body:", body);
+        response.writeHead(200, { "Content-Type": "text/html" });
+      });
+  })
+  .listen(8087);
 
-console.log('server started');
+console.log("server started");
